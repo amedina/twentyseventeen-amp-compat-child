f02222506bae4de49d85425eab77d52ec8b740ba
Alberto Medina <albertomedina@google.com>
2017-06-15 14:33:56 -0700

    First commit of TwentySeventeen AMP Compatible Child Theme

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * The functions.php file used to:
+ *
+ * 1. Set up the features of the theme
+ *    - featured image sizes
+ *    - HTML 5 support
+ *    - Post-formats
+ *    - Configuration of custom menus
+ * 2. Queue all the style sheets and JavaScripts when required.
+ * 3. Hold custom functions, specifically for the theme, or child theme
+ *    - Custom menus, sidebars
+ *    - New functions not present in the parent theme
+ *    - Functions that may override parent functions
+ */
