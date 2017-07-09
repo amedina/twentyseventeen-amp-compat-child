5e0fca741508efbdc05665debba7239d55522b74
Alberto Medina <albertomedina@google.com>
2017-07-09 14:58:24 -0700

    Modify meta-author.php

--- amp/meta-author.php
+++ amp/meta-author.php
@@ -1,15 +1,17 @@
-<?php $post_author = $this->get( 'post_author' ); ?>
+<?php
+    $post_author = $this->get( 'post_author' );
+    $byline = sprintf(
+        /* translators: %s: post author */
+        __( 'by %s', 'amp' ),
+        '<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . $post_author->get( 'display_name' ) . '</a></span>'
+    );
+?>
 <?php if ( $post_author ) : ?>
 	<div class="amp-wp-meta amp-wp-byline">
-		<?php if ( function_exists( 'get_avatar_url' ) ) : ?>
-            <?php
-            $gravatar_url = esc_url( get_avatar_url( $post_author->user_email, array( 'size' => 24 ) ) );
-            $secure_gravtar_url = apply_filters( 'amp_secure_link', $gravatar_url);
-            ?>
-			<amp-img src="<?php echo $secure_gravtar_url ?>" width="24" height="24" layout="fixed"></amp-img>
-		<?php endif; ?>
-		<span class="amp-wp-author author vcard"><?php echo esc_html( $post_author->display_name ); ?></span>
+        <?php
+            echo '<span class="posted-on">' . twentyseventeen_time_link() . '</span><span class="byline"> ' . $byline . '</span>';
+        ?>
 	</div>
 <?php endif; ?>
 
-<!--esc_url( get_avatar_url( $post_author->user_email, array( 'size' => 24 ) ) );-->
+
